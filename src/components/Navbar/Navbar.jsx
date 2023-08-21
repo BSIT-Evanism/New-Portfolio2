bf870ab9619cecd231cc969d646ce7eff669a660
John Evan Solanoy <evanism@apx_managed_aur.vanilla>
2023-08-22 01:06:39 +0800

    added animations to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,24 @@
 import { Link } from "react-router-dom";
 import styles from './Navbar.module.scss'
+import { useState } from "react";
+import { motion } from "framer-motion";
 
 
 function Navbar() {
+  const [hover, setHover] = useState(false)
+
+
   return (
     <>
-      <ul className={styles.nav}>
+      <motion.ul className={styles.nav}
+        initial={{ y: "100vh" }}
+        animate={{ y: 0 }}
+        transition={{ duration: 0.5, ease: "easeIn", delay: 2 }}
+      >
         <Link className={styles.links} to="/"><h3>Home</h3></Link>
         <Link className={styles.links} to="/about"><h3>About</h3></Link>
         <Link className={styles.links} to="/projects"><h3>Projects</h3></Link>
-      </ul>
+      </motion.ul>
     </>
   )
 }
