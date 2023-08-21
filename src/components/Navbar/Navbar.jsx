f397aa1d170ace2ba21c80b3ca84946e8d34d84d
John Evan Solanoy <evanism@apx_managed_aur.vanilla>
2023-08-22 00:40:37 +0800

    redesigned navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom";
+import styles from './Navbar.module.scss'
 
 
 function Navbar() {
   return (
     <>
-      <Link to="/"><h2>Home</h2></Link>
-      <Link to="/about"><h2>About</h2></Link>
-      <Link to="/projects"><h2>Projects</h2></Link>
-
+      <ul className={styles.nav}>
+        <Link className={styles.links} to="/"><h3>Home</h3></Link>
+        <Link className={styles.links} to="/about"><h3>About</h3></Link>
+        <Link className={styles.links} to="/projects"><h3>Projects</h3></Link>
+      </ul>
     </>
   )
 }
