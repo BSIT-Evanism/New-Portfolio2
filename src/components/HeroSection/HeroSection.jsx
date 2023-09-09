3518f36e7729f9a9d34e9c3738956bb4eab08dce
BSIT-Evanism <71175442+BSIT-Evanism@users.noreply.github.com>
2023-09-10 02:44:53 +0800

    fixed translate bug

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,38 +1,40 @@
-import { useState } from "react";
+import {useState} from "react";
 import styles from "./HeroSection.module.scss"
-import { motion } from "framer-motion";
+import {motion} from "framer-motion";
 
 function HeroSection() {
-  const [hover, setHover] = useState(false)
+    const [hover, setHover] = useState(false)
 
-  return (
-    <>
-      <motion.div className={styles.wrapper}
-        initial={{ y: -100 }}
-        animate={{ y: 0 }}
-        whileHover={{ originX: 0.5 }}
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
-        transition={{ duration: 1.5, type: "spring", delay: 0.8 }}
-      >
-        <motion.div className={styles.hero}
-        >
-          <motion.div data-scroll data-scroll-speed="0.1" className={styles.inner}
-          >
-            <motion.div data-scroll data-scroll-speed="0.2" className={styles.float}
-            ></motion.div>
-            <motion.div data-scroll data-scroll-speed="0.1" className={styles.callfloat}
+    return (
+        <>
+            <motion.div className={styles.wrapper}
+                        initial={{y: -100}}
+                        animate={{y: 0}}
+                        whileHover={{originX: 0.5}}
+                        onMouseEnter={() => setHover(true)}
+                        onMouseLeave={() => setHover(false)}
+                        transition={{duration: 1.5, type: "spring", delay: 0.8}}
             >
-              <div className={styles.ping}>
-                <div className={styles.innerPing}/>
-              </div>
+                <motion.div className={styles.hero}
+                >
+                    <motion.div className={styles.inner}
+                    >
+                        <motion.div className={styles.float}
+                                    whileHover={{rotateY: "180deg"}}
+                                    transition={{duration: 0.5}}
+                        ></motion.div>
+                        <motion.div className={styles.callFloat}
+                        >
+                            <div className={styles.ping}>
+                                <div className={styles.innerPing}/>
+                            </div>
 
+                        </motion.div>
+                    </motion.div>
+                </motion.div>
             </motion.div>
-          </motion.div>
-        </motion.div>
-      </motion.div>
-    </>
-  )
+        </>
+    )
 }
 
 export default HeroSection;
