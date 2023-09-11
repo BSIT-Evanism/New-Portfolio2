799c1ad0899a0a5ccdf730d8f1792b39d00208aa
BSIT-Evanism <71175442+BSIT-Evanism@users.noreply.github.com>
2023-09-12 00:36:53 +0800

    cursor fix

--- src/components/Cursor/Cursor.jsx
+++ src/components/Cursor/Cursor.jsx
@@ -1,27 +1,25 @@
-import React from 'react'
-import useMousePosition from '../../utils/useMousePosition'
-import {motion} from 'framer-motion'
-import styles from './Cursor.module.scss'
-import {useHover} from '../../context/ViewContext'
-
+import React from "react";
+import useMousePosition from "../../utils/useMousePosition";
+import { motion } from "framer-motion";
+import styles from "./Cursor.module.scss";
+import { useHover } from "../../context/ViewContext";
 
 export default function Cursor() {
-    const hover  = useHover()
-    const {mousePosition} = useMousePosition()
+  const hover = useHover();
+  const { mousePosition } = useMousePosition();
 
-    
-    const size = hover ? 3 : 0.8;
+  const size = hover ? 3 : 0.8;
   return (
     <>
-{!hover && (
-      <motion.div
-layoutId="logoCursor"
-        className={styles.cursor}
-        style={{x: mousePosition.x, y: mousePosition.y}}
-                    animate={{scale: size}}
-        transition={{type: "tween", ease: "backOut"}}
-      ></motion.div>
-)}
+      {!hover && (
+        <motion.div
+          layoutId="logoCursor"
+          className={styles.cursor}
+          style={{ x: mousePosition.x, y: mousePosition.y }}
+          animate={{ scale: size }}
+          transition={{ type: "tween", ease: "backOut" }}
+        ></motion.div>
+      )}
     </>
   );
 }
